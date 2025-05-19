786bde5ccb85165c876b4e704f0a9fe4bef523fa
Shaun Rudisail <sprudisail@gmail.com>
2025-05-19 19:46:18 -0400

    Fixed all errors.

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Xml.Serialization;
+
 namespace ChristmasWithTypes
 {
     public class Christmas
     {
-        public string[] Presents;
+        public string [] Presents;
         public string Santa { get; set; }
-        public int TreeHeight { get; set; } //TODO Make the Height property nullable
+        public int? TreeHeight { get; set; } //TODO Make the Height property nullable
 
         //TODO Make the property, "Day", type enum
-        public enum Day { /*Fill out the days of the week*/ };
+        public enum Day { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
 
     }
 }
